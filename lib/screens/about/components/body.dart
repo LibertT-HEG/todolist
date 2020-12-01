2ef74bd580ea3feff8a70df807aabdf4b04cb8c9
ANTOINE FREY <antoine.frey@he-arc.ch>
2020-12-01 17:40:11 +0100

    Ajout d'une image dans About

---
+++ lib/screens/about/components/body.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: new Row(
+        children: <Widget>[
+          new Container(
+            child: new Image.asset('assets/images/logo.png', height: 200.0,),
+          ),
+          new Container(
+            child: new Text('long information text'),
+          ),
+        ],
+      ),
+    );
+  }
+}
