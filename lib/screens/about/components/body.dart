2a3e57a2a8b9f0cb6db675df1f3dbdbb97bf86cc
ANTOINE FREY <antoine.frey@he-arc.ch>
2020-12-08 13:30:58 +0100

    Test de mise en page pour about

--- lib/screens/about/components/body.dart
+++ lib/screens/about/components/body.dart
@@ -3,17 +3,26 @@ import 'package:flutter/material.dart';
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: new Row(
-        children: <Widget>[
-          new Container(
-            child: new Image.asset('assets/images/logo.png', height: 200.0,),
+    return SafeArea(
+        child: new Container(
+          alignment: Alignment.center,
+          child: Column(
+            children: <Widget>[
+              Center(
+                child: Text("ToutDoux Liste", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+              ),
+              Center(
+                child: Text("Version 0.1", style: TextStyle(fontSize: 18, )),
+              ),
+              Center(
+                child: new Image.asset('assets/images/logo.png', height: 200.0,),
+              ),
+              Center(
+                child: Text('ToutDoux Liste est application de prise de note \n\n Version 0.1', textAlign: TextAlign.center,),
+              ),
+            ],
           ),
-          new Container(
-            child: new Text('long information text'),
-          ),
-        ],
-      )
+        )
     );
   }
 }
