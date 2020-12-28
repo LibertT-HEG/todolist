08d73a087c6109c9626433b89d96d8a1cbf825ff
ga-et <56021069+ga-et@users.noreply.github.com>
2020-12-28 13:57:26 +0100

    delete addList.dart devenu inutile

--- lib/screens/list_add/components/MyCustomForm.dart
+++ lib/screens/list_add/components/MyCustomForm.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:todolist/screens/list_add/components/addList.dart';
 
 // Define a custom Form widget.
 class MyCustomForm extends StatefulWidget {
